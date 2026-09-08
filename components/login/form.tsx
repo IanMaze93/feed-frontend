cd18662ffb1e2344aec7ffa5fe5819d841763617
Ian Maze <mazeiandev@outlook.com>
2026-09-08 17:04:46 -0400

    feat(styling): [FF-2] add more styling

--- components/login/form.tsx
+++ components/login/form.tsx
@@ -2,7 +2,7 @@ export default function LoginForm() {
   return (
     <form className="flex gap-4 self-center rounded-md border-2 border-[#ff6a00] p-4 shadow-md flex-col items-start">
       <div className="flex items-center">
-        <label className="w-21">UserName:</label>
+        <label className="w-25 font-bold">UserName:</label>
         <input
           className="rounded border-1 border-[#ff6a00]"
           type="text"
@@ -11,7 +11,7 @@ export default function LoginForm() {
       </div>
 
       <div className="flex items-center">
-        <label className="w-21">Password:</label>
+        <label className="w-25 font-bold">Password:</label>
         <input
           className="rounded border-1 border-[#ff6a00]"
           type="password"
@@ -20,7 +20,7 @@ export default function LoginForm() {
       </div>
 
       <button
-        className="self-center rounded border-1 border-[#ff6a00] p-2 hover:bg-[#ff6a00] hover:text-white"
+        className="self-center rounded border-1 border-[#ff6a00] p-2 hover:bg-[#ff6a00] font-bold hover:text-white"
         type="submit"
       >
         Login
